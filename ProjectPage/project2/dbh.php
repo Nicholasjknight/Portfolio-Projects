571b5151e6503ff0ecc4a17ccbdcecea06093296
Nicholasjknight <nicholas.jknight@yahoo.com>
2023-08-31 20:37:19 -0400

    Add files via upload

---
+++ ProjectPage/project2/dbh.php
@@ -0,0 +1,9 @@
+<?php
+
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "dbphpsearch";
+$conn = mysqli_connect($server, $username, $password, $dbname); 
+?>
+
---
+++ ProjectPage/project2/header.php
@@ -0,0 +1,11 @@
+<?php
+    include 'dbh.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="php.css">
+</head>
+<body>

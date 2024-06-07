a848c10537ff3352fe2a78323bb221985fc0e047
Nicholasjknight <nicholas.jknight@yahoo.com>
2024-06-07 01:25:38 -0400

    Update project.js

--- PortfolioPage/project.js
+++ PortfolioPage/project.js
@@ -1,62 +1,42 @@
-"version:"; "0.2.0",
-"configurations:" [
-    {
-        "type": "chrome",
-        "request": "launch",
-        "name": "Launch Chrome against localhost",
-        "url": "http://localhost:8080",
-        "webRoot": "./projects.html"
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const query = document.getElementById('searchInput').value;
+    if (query) {
+        alert(`Searching for: ${query}`);
+    } else {
+        alert('Please enter a search term.');
     }
-]
+});
 
-
-    var video_player = document.getElementById("video_player"),
-    links = video_player.getElementsByTagName('a');
-    for (var i=0; i<links.length; i++) {
-        links[i].onclick = handler;
-    }
-    function handler(e) {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
         e.preventDefault();
-        videotarget = this.getAttribute("href");
-        filename = videotarget.substr(0, videotarget.lastIndexOf('.')) || videotarget;
-        video = document.querySelector("#video_player video");
-        video.removeAttribute("poster");
-        source = document.querySelectorAll("#video_player video source");
-        source[0].src = filename + ".mp4";
-        source[1].src = filename + ".webm";
-        video.load();
-        video.play();    
-    }
-    function fullscreen(){
-        var fscreen = document.getElementById('video_player');
-        if (fscreen.requestFullscreen){
-            fscreen.requestFullscreen();
-        } else if (elem.webkitRequestFullscreen) { /* Safari */
-        elem.webkitRequestFullscreen();
-      } else if (elem.msRequestFullscreen) { /* IE11 */
-        elem.msRequestFullscreen();
-      }
-    }
-    function wheel(event) {
-        var delta = 0;
-        if (event.wheelDelta) {(delta = event.wheelDelta / 200);}
-        else if (event.detail) {(delta = -event.detail / 1);}
-  
-        handle(delta);
-        if (event.preventDefault) {(event.preventDefault());}
-        event.returnValue = false;
-    }
-  
-    function handle(delta) {
-        var time = 500;
-        var distance = 700;
-  
-        $('html, body').stop().animate({
-            scrollTop: $(window).scrollTop() - (distance * delta)
-        }, time );
-    }
-  
-    if (window.addEventListener) {window.addEventListener('DOMMouseScroll', wheel, false);}
-      window.onmousewheel = document.onmousewheel = wheel;
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
 
+function scrollToTop() {
+    window.scrollTo(0, 0);
+}
+
+window.addEventListener('scroll', function() {
+    const scrolled = window.scrollY;
+    const rotatingBackground = document.querySelector('.rotating-background');
+    const currentRotation = parseFloat(rotatingBackground.style.transform.match(/rotate\((-?\d+\.?\d*)deg\)/)[1]);
+    const currentScale = parseFloat(rotatingBackground.style.transform.match(/scale\((-?\d+\.?\d*)\)/)[1]);
+
+    if (currentRotation >= 350.6 && currentScale >= 2.753) {
+        rotatingBackground.style.animation = 'none';
+    } else {
+        rotatingBackground.style.animation = 'spinZoom 20s linear infinite';
+    }
+
+    rotatingBackground.style.transform = `scale(${1 + scrolled / 1000}) rotate(${scrolled / 5}deg)`;
+
+    document.querySelectorAll('.parallax').forEach(parallax => {
+        parallax.style.backgroundPositionY = `${scrolled * 0.5}px`;
+    });
+});
 
